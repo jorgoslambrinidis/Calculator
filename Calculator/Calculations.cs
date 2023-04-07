1015ee97c48009232ca1f5780502c79830a04fa8
jorgoslambrinidis <jorgos.lambrinidis@gmail.com>
2023-04-07 20:51:33 +0200

    Add project files.

---
+++ Calculator/Calculations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculations
+    {
+        public void Addition(double num1, double num2)
+        {
+            var result = num1 + num2;
+            Console.WriteLine($"Operacija Sobiranje: {result}");
+        }
+                                          //5           //3
+        public double AdditionPlus(double num1, double num2)
+        {
+            num1 = num1 + 1.5; // num1 += 1.5;
+            var result = num1 + num2;
+            return result; //9.5
+        }
+
+        public void Substruction(double num1, double num2)
+        {
+            // num1 -= 1.5;
+            var result = num1 - num2;
+            Console.WriteLine($"Operacija Odzemanje: {result}");
+        }
+
+        public void Multiplication(double num1, double num2)
+        {
+            var result = num1 * num2;
+            Console.WriteLine($"Operacija Mnozenje: {result}");
+        }
+
+        public void Division(double num1, double num2)
+        {
+            var result = num1 / num2;
+            Console.WriteLine($"Operacija Delenje: {result}");
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using Calculator;
+
+Initialization initialization = new Initialization();
+initialization.Init();
+
+Calculations calculations = new Calculations();
+
+Console.WriteLine("Enter first number:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter second number:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("***************************************************\n");
+Console.WriteLine("Enter symbol(/,+,-,*):\n");
+
+string symbol = Console.ReadLine();
+Console.WriteLine("***************************************************\n");
+
+
+switch (symbol)
+{
+    case "+": calculations.Addition(num1, num2); break;
+
+    case "-": calculations.Substruction(num1, num2); break;
+
+    case "*": calculations.Multiplication(num1, num2); break;
+
+    case "/": calculations.Division(num1, num2); break;
+
+    default: Console.WriteLine("Nepravilna Operacija"); break;
+}
+
+Console.ReadLine();
+
+
+
+//if (symbol == "+")
+//{
+//    // sobiranje
+//    Console.WriteLine($"Operacija Sobiranje: {num1 + num2}");
+//}
+//else if (symbol == "-")
+//{
+//    // odzemanje
+//    var odzemanjeRez = num1 - num2;
+//    Console.WriteLine($"Operacija Odzemanje: {odzemanjeRez}");
+//}
+//else if (symbol == "*")
+//{
+//    // mnozenje
+//    var mnozenjeRez = num1 * num2;
+//    Console.WriteLine($"Operacija Mnozenje: {mnozenjeRez}");
+
+//}
+//else if (symbol == "/")
+//{
+//    // delenje
+//    var delenjeRez = num1 / num2;
+//    Console.WriteLine($"Operacija delenje: {delenjeRez}");
+//}
+//else
+//{
+//    Console.WriteLine("Nepravilna Operacija");
+//}
+
+
